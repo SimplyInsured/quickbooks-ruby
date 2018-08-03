9c1d4af7818afffdb1ffe3bb6bda1b9de5b99d13
David Bai <valcrist77@gmail.com>
2018-08-02 22:25:17 -0700

    Update entitlements.rb

--- lib/quickbooks/service/entitlements.rb
+++ lib/quickbooks/service/entitlements.rb
@@ -4,7 +4,7 @@ module Quickbooks
       
       def url_for_base
         raise MissingRealmError.new unless @company_id
-        "#{@base_uri.gsub('/v3/company', '/entitlements/v3')}/#{@company_id}"
+        "https://qbo.sbfinance.intuit.com/manage/entitlements/v3/#{@company_id}"
       end
       
       private
