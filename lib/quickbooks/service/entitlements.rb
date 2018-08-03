c5664eeacc66f609c9bc713491f86652162d1079
David Bai <valcrist77@gmail.com>
2018-08-02 22:14:04 -0700

    Update entitlements.rb

--- lib/quickbooks/service/entitlements.rb
+++ lib/quickbooks/service/entitlements.rb
@@ -1,7 +1,12 @@
 module Quickbooks
   module Service
     class Entitlements < BaseService
-
+      
+      def url_for_base
+        raise MissingRealmError.new unless @company_id
+        "#{@base_uri.gsub('/v3/company', '/entitlements/v3')}/#{@company_id}"
+      end
+      
       private
 
       def model
